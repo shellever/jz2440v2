f3d12474b72718e8fbc949cbf54c712019f6a75a
shellever <shellever@163.com>
2019-03-04 01:53:31 +0800

    add tools: mkimage

---
+++ tools/mkimage/envsetup-uboot.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# usage:
+# source envsetup-uboot.sh
+
+# store the current script root path
+Q_UBOOT_PATH_CUR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
+# store the current working directory
+Q_WORK_DIR_CUR=$(pwd)
+
+
+# ===>enter
+cd $Q_UBOOT_PATH_CUR
+
+UBOOT_TOOLS_PATH="$Q_UBOOT_PATH_CUR/u-boot-1.1.6"
+export PATH=$UBOOT_TOOLS_PATH:$PATH
+
+# other way
+# sudo apt-get install u-boot-tools
+
+# <===exit
+cd $Q_WORK_DIR_CUR
+
+# exported cmds
+# mkimage
+
