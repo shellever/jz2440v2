d3a55dc70b48b54e212c03ccf8af0b98d4750dca
shellever <shellever@163.com>
2019-03-04 01:06:37 +0800

    add tools: jlink

---
+++ tools/jlink/README.md
@@ -0,0 +1,27 @@
+# JLink
+
+```
+SEGGER - JLink
+https://www.segger.com/downloads/jlink
+J-Link Software and Documentation Pack[Beta version]/
+J-Link Software and Documentation pack for Linux, DEB installer, 64-bit [Beta]
+https://www.segger.com/downloads/jlink/#J-LinkSoftwareAndDocumentationPackBeta
+
+
+installation
+1. extract
+$ tar -zxvf JLink_Linux_V635g_x86_64.tgz
+
+2. read doc
+$ vi README.txt
+
+3. install
+$ cd JLink_Linux_V635g_x86_64
+$ sudo cp -p 99-jlink.rules /etc/udev/rules.d/
+$ cp -Rp./* ~/devsoft/tools/jlink/JLink_Linux_V635g_x86_64
+
+4. testing
+$ ./JLinkExe
+```
+
+
---
+++ tools/jlink/envsetup-jlink.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# usage:
+# source envsetup-jlink.sh
+
+# store the current script root path
+Q_JLINK_PATH_CUR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
+# store the current working directory
+Q_WORK_DIR_CUR=$(pwd)
+
+
+JLINK_V635G="JLink_Linux_V635g_x86_64"
+JLINK_V500E="JLink_Linux_V500e_x86_64"
+JLINK_V=$JLINK_V500E
+
+# ===>enter
+cd $Q_JLINK_PATH_CUR
+
+if [ ! -d $JLINK_V ]; then
+    tar -zxvf $JLINK_V.tgz
+fi
+
+if [ ! -f /etc/udev/rules.d/99-jlink.rules ]; then
+    sudo cp -fp $JLINK_PATH/99-jlink.rules /etc/udev/rules.d/
+    # sudo reboot
+fi
+
+JLINK_PATH="$Q_JLINK_PATH_CUR/$JLINK_V"
+export PATH=$JLINK_PATH:$PATH
+
+# <===exit
+cd $Q_WORK_DIR_CUR
+
