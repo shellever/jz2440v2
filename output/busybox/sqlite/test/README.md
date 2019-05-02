c07b97676f554b2be68569f71d1bff5e54b6b024
shellever <shellever@163.com>
2019-05-02 15:23:35 +0800

    add readme for sqlite testing

---
+++ output/busybox/sqlite/test/README.md
@@ -0,0 +1,18 @@
+## 编译测试
+
+```
+编译SQLite测试程序
+$ make && make test
+
+
+运行测试脚本
+/test/sqlite # ./sqlite-test.sh                                                 
+name = shellever                                                                
+age = 18                                                                        
+                                                                                
+name = linuxfor                                                                 
+age = 20                                                                        
+
+```
+
+
--- output/busybox/sqlite/test/sqlite-test.sh
+++ output/busybox/sqlite/test/sqlite-test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-SQLITE_TEST=sqlite-test.out
+
 SQLITE_DB=test-out.db
-./$SQLITE_TEST $SQLITE_DB "create table person(name varchar(20), age int);"
-./$SQLITE_TEST $SQLITE_DB "insert into person values('shellever', 18);"
-./$SQLITE_TEST $SQLITE_DB "insert into person values('linuxfor', 20);"
-./$SQLITE_TEST $SQLITE_DB "select * from person;"
+
+rm -rf $SQLITE_DB
+
+./sqlite-test.out $SQLITE_DB "create table person(name varchar(20), age int);"
+./sqlite-test.out $SQLITE_DB "insert into person values('shellever', 18);"
+./sqlite-test.out $SQLITE_DB "insert into person values('linuxfor', 20);"
+./sqlite-test.out $SQLITE_DB "select * from person;"
